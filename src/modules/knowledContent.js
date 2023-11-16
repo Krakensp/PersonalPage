b689793570a03951c3b6def117b5bd7889afe202
Krakensp <krakenspeare@hotmail.com>
2023-11-16 10:06:58 -0600

    styles of knowleds changed, intro changed

--- src/modules/knowledContent.js
+++ src/modules/knowledContent.js
@@ -6,9 +6,9 @@ const displayStars = (porcentage=>{
     let starsDisplay = "";
     for (let i=0; i < 10; i++){
       if(i < stars){
-        starsDisplay+= "⭐"
-      }else{
         starsDisplay+= "★"
+      }else{
+        starsDisplay+= "☆"
       }
     }
 
@@ -30,6 +30,18 @@ const getKnowled = (data, selectedTab, $knowleds)=>{
             let element =  d.createElement("li");
             element.innerHTML="<span>"+ el.name+"</span>" + "<span class='star'>" + displayStars(el.porcentage)+"</span>";
             $fragment.appendChild(element);
+            
+        
+                const $tabs = d.querySelectorAll(".table-btn");
+                $tabs.forEach(tab=>{
+                  if(tab.dataset.knowled === selectedTab){
+                     tab.classList.add("btn-is-active");
+                     tab.disabled = true;
+                  }else{
+                    tab.classList.remove("btn-is-active");
+                     tab.disabled = false;
+                  }
+                });
         }
     });
 
